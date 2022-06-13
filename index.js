e1d7314849b69f6f51480643aa0130628e9c2646
Nii <bnstephens001@gmail.com>
2022-06-13 08:51:11 +0000

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+function saturdayFun(activity='roller-skate'){
+        return `This Saturday, I want to ${activity}!`;
+}
+saturdayFun('bathe my dog')
+
+function mondayWork(activty = 'go to the office'){
+    return (`This Monday, I will ${activty}.`)
+}
+mondayWork('work from home')
+
+
+function wrapAdjective(result = '*') { 
+    // let emphatic = 'You are'
+    return function (inner = "handsome"){
+       return  `You are ${result}${inner}${result}!`
+    }
+}
+
+    // return function (result = '*') {
+    //    
